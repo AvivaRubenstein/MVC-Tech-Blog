18a60a9e42c978aec736df2c70df2dec351128d3
AvivaRubenstein <avivadrubenstein@gmail.com>
2023-02-20 21:25:52 -0500

    set up logout button (still not functional to do post request to delete session)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -61,6 +61,7 @@ res.render('login');
 
 
 
+
 module.exports = router;
 
 
@@ -85,3 +86,25 @@ module.exports = router;
 //set up button to delete past posts
 
 
+//get an individual post
+// router.get('/project/:id', async (req, res) => {
+//     try {
+//       const projectData = await Project.findByPk(req.params.id, {
+//         include: [
+//           {
+//             model: User,
+//             attributes: ['name'],
+//           },
+//         ],
+//       });
+  
+//       const project = projectData.get({ plain: true });
+  
+//       res.render('project', {
+//         ...project,
+//         logged_in: req.session.logged_in
+//       });
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
